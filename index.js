9e2f31107ad00045297306b03faf910791af2efa
Ajlan-Khan <Ajlan-khan23@gmail.com>
2022-10-05 21:34:13 +0600

    make it better

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app=require('./app')
 require('dotenv').config()
 const port=process.env.PORT || 4000
 app.listen(port,()=>{
-    console.log(`Server is running at http://localhost:${port}`)
+    console.log("Server is listening to port"+port)
 })
